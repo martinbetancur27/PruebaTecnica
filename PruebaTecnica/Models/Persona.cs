28d1c74f0332049db55fd27261583f0af8720381
Martin Betancur <martinbetancur27@gmail.com>
2023-03-17 14:03:03 -0500

    Añadir modelo Persona

---
+++ PruebaTecnica/Models/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PruebaTecnica.Models
+{
+    public class Persona
+    {
+        public string Nombres { get; set; }
+
+        public string Apellidos { get; set; }
+
+        [Key]
+        public string Identificacion { get; set; }
+
+        public string Genero { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string CorreoElectronico { get; set; }
+    }
+}
