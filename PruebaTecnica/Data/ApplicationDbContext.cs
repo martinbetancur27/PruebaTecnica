5bfcf0811cc47b6d3344f487c6a3c1a5158f6993
Martin Betancur <martinbetancur27@gmail.com>
2023-03-17 14:19:51 -0500

    Instalar EF Core y crear contexto de base de datos

---
+++ PruebaTecnica/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PruebaTecnica.Models;
+
+namespace PruebaTecnica.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Persona> Personas { get; set; }
+
+    }
+}
